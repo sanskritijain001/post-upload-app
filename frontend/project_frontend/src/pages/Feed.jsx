f108fe23cc2027fc8b67a2c94b4b58107c450290
sanskritijain001 <jainsanskriti1802@gmail.com>
2026-06-25 09:48:02 +0530

    Inital commit - full stack post upload app

---
+++ frontend/project_frontend/src/pages/Feed.jsx
@@ -0,0 +1,45 @@
+import React, { useState,useEffect } from 'react'
+import axios from "axios"
+
+const Feed = () => {
+    const [posts, setPosts] = useState([
+        {
+            _id:"1",
+            image: "https://tse4.mm.bing.net/th/id/OIP.hXWwNOQw15ZVWKlMs-xv0wHaFQ?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+            caption: "beautiful flower",
+        }
+    ])
+
+    useEffect( ()=>{
+        axios.get("http://localhost:3000/posts")
+        .then((res) =>{
+            console.log(res.data.posts)
+            setPosts(res.data.posts)
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
+
+    },[])
+
+    return (
+        <section className='feed-section'>
+            {
+                posts.length > 0 ?(
+                    posts.map( (post) =>(
+                        <div key={post._id} className='post-card'>
+                            <img src={post.image} alt={post.caption}/>
+                            <p>{post.caption}</p> 
+
+                        </div>
+                    ))
+                ):(
+                    <h1>No posts available</h1>
+                )
+            }
+
+        </section>
+    )
+}
+
+export default Feed
