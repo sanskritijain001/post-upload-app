26aae6ff3610421f10da019296c68a3823c45c4b
sanskritijain001 <jainsanskriti1802@gmail.com>
2026-07-02 10:32:46 +0530

    Import Navigate and fix routing

--- frontend/project_frontend/src/App.jsx
+++ frontend/project_frontend/src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import CreatePost from './pages/CreatePost.jsx'
-import Feed from './pages/Feed.jsx'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
+import CreatePost from './pages/CreatePost.jsx';
+import Feed from './pages/Feed.jsx';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-         <Route path="/" element={<Navigate to="/feed" replace />} />
-        <Route path='/create-post' element={ <CreatePost/>} />
-        <Route path='/feed' element={<Feed/>} />
+        <Route path="/" element={<Navigate to="/feed" replace />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
