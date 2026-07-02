e7dcb8320ea063600b80201804c9d568d3b2681a
sanskritijain001 <jainsanskriti1802@gmail.com>
2026-07-02 10:29:09 +0530

    Fix root route

--- frontend/project_frontend/src/App.jsx
+++ frontend/project_frontend/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
+         <Route path="/" element={<Navigate to="/feed" replace />} />
         <Route path='/create-post' element={ <CreatePost/>} />
         <Route path='/feed' element={<Feed/>} />
       </Routes>
